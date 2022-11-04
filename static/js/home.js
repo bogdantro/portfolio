742ffd65fd3b6ec1827390e8b65ff1cf7481e705
bogdantro <sabertoothtri@gmail.com>
2022-11-04 23:14:25 +0200

    fg

--- static/js/home.js
+++ static/js/home.js
@@ -1,24 +1,24 @@
-// window.onload = function(){ 
+window.onload = function(){ 
 
-//     const loader = document.getElementById('loader');
+    const loader = document.getElementById('loader');
 
 
-//     if ($(window).width() > 1150) {
-//         setTimeout(() => {
-//             loader.classList.add('hidden');
-//         }, 1500);
-//         setTimeout(() => {
-//             document.getElementById('joke').classList.add('active')
-//         }, 2000);
-//     }else{
-//         setTimeout(() => {
-//             document.getElementById('joke').classList.add('active')
-//         }, 1500);
-//         setTimeout(() => {
-//             loader.classList.add('hidden');
-//         }, 2000);
-//     }
-// }
+    if ($(window).width() > 1150) {
+        setTimeout(() => {
+            document.getElementById('joke').classList.add('active')
+        }, 1800);
+        setTimeout(() => {
+            loader.classList.add('hidden');
+        }, 2300);
+    }else{
+        setTimeout(() => {
+            document.getElementById('joke').classList.add('active')
+        }, 1500);
+        setTimeout(() => {
+            loader.classList.add('hidden');
+        }, 2000);
+    }
+}
 
 
 
