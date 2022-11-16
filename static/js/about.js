a3fa3de8a92d467652591b7ad027205bbc4d57b4
bogdantro <sabertoothtri@gmail.com>
2022-11-16 21:07:41 +0200

    df

--- static/js/about.js
+++ static/js/about.js
@@ -1,22 +1,39 @@
-const mygoal = document.getElementById('myGoalCard')
-const arrowBackCard = document.getElementById('arrowBackCard')
-
 function myGoal(){
     const mygoal = document.getElementById('myGoalCard')
-    const arrowBackCard = document.getElementById('arrowBackCard')
+    const arrowBackCard = document.getElementById('arrowBackCard1')
 
     mygoal.classList.add('active');
 
     setTimeout(() => {
         arrowBackCard.classList.add('active');
-    }, 400);
+    }, 350);
 }
 
 function myGoalClose(){
     const mygoal = document.getElementById('myGoalCard')
-    const arrowBackCard = document.getElementById('arrowBackCard')
+    const arrowBackCard = document.getElementById('arrowBackCard1')
 
     mygoal.classList.remove('active');
+
+    arrowBackCard.classList.remove('active');
+}
+
+function infoAbtMe(){
+    const infoAbtMe = document.getElementById('infoAbtMeCard')
+    const arrowBackCard = document.getElementById('arrowBackCard2')
+
+    infoAbtMe.classList.add('active');
+
+    setTimeout(() => {
+        arrowBackCard.classList.add('active');
+    }, 350);
+}
+
+function infoAbtMeClose(){
+    const infoAbtMe = document.getElementById('infoAbtMeCard')
+    const arrowBackCard = document.getElementById('arrowBackCard2')
+
+    infoAbtMe.classList.remove('active');
 
     arrowBackCard.classList.remove('active');
 }
