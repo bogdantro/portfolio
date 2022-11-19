91690f8c2414584eeba9b624cecccafc27b5c09a
bogdantro <sabertoothtri@gmail.com>
2022-11-19 13:08:46 +0200

    sd

--- static/js/about.js
+++ static/js/about.js
@@ -37,3 +37,29 @@ function infoAbtMeClose(){
 
     arrowBackCard.classList.remove('active');
 }
+
+function whatIDo(){
+    const whatIDo = document.getElementById('whatIDoCard')
+    const arrowBackCard = document.getElementById('arrowBackCard3')
+    const bottomCover =  document.getElementById('bottomCover')
+
+    whatIDo.classList.add('active');
+
+    bottomCover.style.display = 'none';
+
+    setTimeout(() => {
+        arrowBackCard.classList.add('active');
+    }, 350);
+}
+
+function whatIDoClose(){
+    const whatIDo = document.getElementById('whatIDoCard')
+    const arrowBackCard = document.getElementById('arrowBackCard3')
+    const bottomCover =  document.getElementById('bottomCover')
+
+    whatIDo.classList.remove('active');
+
+    bottomCover.style.display = 'unset';
+
+    arrowBackCard.classList.remove('active');
+}
